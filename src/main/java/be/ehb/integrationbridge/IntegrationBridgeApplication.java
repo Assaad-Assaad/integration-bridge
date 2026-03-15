c59b3a56e08cc2522e4587756b4ddf219b9c9e2f
Assaad <assaad.assaad@student.ehb.be>
2026-03-15 17:38:01 +0100

    Initial Spring Boot project setup with dependencies and package structure

---
+++ src/main/java/be/ehb/integrationbridge/IntegrationBridgeApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.integrationbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/integrationbridge/IntegrationBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.integrationbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
